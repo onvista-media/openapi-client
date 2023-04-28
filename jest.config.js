1390eb53b0a9f197cde6569bbd02d4f60748f60d
Oliver Weber <ow@uix.io>
2023-04-28 21:18:37 +0200

    cleanup test

--- jest.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} */
-module.exports = {
-  preset: "ts-jest",
-  testEnvironment: "node",
-  testPathIgnorePatterns: ["<rootDir>/node_modules", "<rootDir>/dist"],
-  testRegex: "(/__tests__/.*| (\\.| /)(test|spec))\\.ts?$",
-};
